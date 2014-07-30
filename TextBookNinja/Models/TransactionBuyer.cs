75f124cf3239cc744a7ebc487f7b3410015dd414
Tyler Buchanan <tyler@buchanansystems.com>
2014-07-30 00:00:49 -0500

    fix to TransactionBuyer on namespace issue.

--- TextBookNinja/Models/TransactionBuyer.cs
+++ TextBookNinja/Models/TransactionBuyer.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
-using TextBookNinja.Models;
+using IdentitySample.Models;
 
 namespace OnlineBookWebsite.Models
 {
